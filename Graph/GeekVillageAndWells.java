cda438635ac125703221d5d929d9f35d8fcba721
Rohit-Naikade <naikaderohit833@gmail.com>
2025-03-05 22:00:09 +0530

    DSA : 05:03:25

--- Graph/GeekVillageAndWells.java
+++ Graph/GeekVillageAndWells.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.Scanner;
+
+import javax.naming.LinkException;
 
 class Pair{
     int x, y;
@@ -8,7 +11,7 @@ class Pair{
     }
 }
 
-public class RottingOranges {
+public class GeekVillageAndWells {
 
     static int dx[] = { -1, 0, 1, 0 };
     static int dy[] = { 0, -1, 0, 1 };
@@ -20,59 +23,69 @@ public class RottingOranges {
         System.out.println("Enter no of cols:");
         int cols = sc.nextInt();
 
-        int matrix[][] = new int[rows][cols];
+        char matrix[][] = new char[rows][cols];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.println("Enter value for " + i + " and " + j);
-                matrix[i][j] = sc.nextInt();
+                matrix[i][j] = sc.next().charAt(0);
             }
         }
+        int ans[][]=new int[rows][cols];
+        chefAndWells(rows, cols, matrix, ans);
 
-        System.out.println("Minimum no. of seconds: " + orangesRotting(matrix));
+        System.out.println("Answer is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 
-    private static int orangesRotting(int[][] matrix) {
-        Queue<Pair> queue=new LinkedList<>();
-        int ans=0;
+    private static void chefAndWells(int rows, int cols, char[][] matrix, int ans[][]) {
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==2){
+        
+        Queue<Pair> queue=new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(matrix[i][j]=='W'){
                     queue.add(new Pair(i, j));
                 }
             }
         }
 
-        while(!queue.isEmpty()){
+        int level=1;
 
+        while(!queue.isEmpty()){
             int size=queue.size();
-            int temp=0;
+
             while(size>0){
                 Pair tmp=queue.remove();
-                matrix[tmp.x][tmp.y]=2;
+
                 for(int i=0;i<4;i++){
                     int ii=tmp.x+dx[i];
                     int ij=tmp.y+dy[i];
 
-                    if(ii>=0 && ij>=0 && ii<matrix.length && ij<matrix[0].length && matrix[ii][ij]==1){
-                        temp=1;
+                    if(ii>=0 && ij>=0 && ii<rows && ij<cols && (matrix[ii][ij]=='H' || matrix[ii][ij]=='.')){
+                        ans[ii][ij]=level*2;
+                        matrix[ii][ij]='N';
                         queue.add(new Pair(ii, ij));
                     }
                 }
                 size--;
             }
-            ans=ans+temp;
+
+            level++;
         }
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==1){
-                    ans=-1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(matrix[i][j]=='H'){
+                    ans[i][j]=-1;
                 }
             }
         }
-
-        return ans;
     }
 }
